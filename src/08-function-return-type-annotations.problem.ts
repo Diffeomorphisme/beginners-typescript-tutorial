5fdee4f30843e67d165fb4ff70bc43eec209a88b
Diffeomorphisme <nicosfaction@msn.com>
2023-12-19 09:46:17 +0100

    feat(exercices): completed all exercices

--- src/08-function-return-type-annotations.problem.ts
+++ src/08-function-return-type-annotations.problem.ts
@@ -17,8 +17,19 @@ interface Post {
  * How do we ensure that makeUser ALWAYS
  * returns a user?
  */
-const makeUser = () => {
-  return {};
+const makeUser = (): User => {
+  return {
+    id: 1,
+    firstName: "nicolas",
+    lastName: "John",
+    role: "admin",
+    posts: [
+      {
+        id: 1,
+        title: "lol"
+      }
+    ]
+  };
 };
 
 it("Should return a valid user", () => {
--- src/18-function-types-with-promises.problem.ts
+++ src/18-function-types-with-promises.problem.ts
@@ -4,11 +4,12 @@ interface User {
   id: string;
   firstName: string;
   lastName: string;
+
 }
 
 const createThenGetUser = async (
-  createUser: unknown,
-  getUser: unknown,
+  createUser: () => Promise<string>,
+  getUser: (userId: string) => Promise<User>,
 ): Promise<User> => {
   const userId: string = await createUser();
 
